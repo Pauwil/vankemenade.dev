71dbc6ca3f0056bbbb92e85c1d249134649ca2dd
Paul Kuijten <paul.kuijten@gmail.com>
2014-03-14 23:13:33 +0100

    no message

---
+++ inschrijven-wijzing.php
@@ -0,0 +1,273 @@
+<!-- ######################## Section ######################## -->
+
+<section>
+
+  <div class="section_main">
+   
+      <div class="row">
+
+          <section class="four columns">
+              <div class="panel">
+
+                  <h3>Navigatie</h3>
+                  <ul class="nav-bar vertical">
+                      <li class="active"><a href="index.php?p=rondleiding.php">Rondleiding</a></li>
+                      <li><a href="index.php?p=vestiging.php">Vestiging</a></li>
+                      <li><a href="index.php?p=team.php">Ons Team</a></li>
+                  </ul>
+
+              </div>
+          </section>
+      
+          <section class="eight columns">
+          
+              <h2>Inschrijven & Wijzigen</h2>
+
+              <article class="blog_post">
+
+
+                      <table>
+                          <tr>
+                              <td>
+                                  <table>
+                                      <tr>
+                                          <td>
+                                              <span>Door onderstaand formulier in te vullen kunt u uw gegevens doorgeven aan de praktijk. Wilt u een wijziging doorgeven van uw adres of uw verzekering dan kan dat ook hier. Voor ieder gezinslid dient u een apart formulier in te vullen. Uw gegevens worden vertrouwelijk behandeld.</span>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>U kunt het ingevulde formulier printen en bij uw bezoek meebrengen of aan ons versturen per mail.</span>
+                                          </td>
+                                      </tr>
+                                  </table>
+
+                                  <table>
+                                      <tr>
+                                          <td>
+                                              <span style='font-weight:bold;'>&nbsp;</span>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span style='font-weight:bold;'>Persoonsgegevens</span>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span >Soort melding </span>
+                                          </td>
+                                          <td>
+                                              <select name=Insc size=1>
+                                                  <option selected value=Inschrijven>Inschrijven</option>
+                                                  <option value=Wijzigen>Wijzigen</option>
+                                              </select>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Achternaam </span>
+                                          </td>
+                                          <td>
+                                              <input name=Achternaam maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Voorletters</span>
+                                          </td>
+                                          <td>
+                                              <input name=Voorletters maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Voornaam</span>
+                                          </td>
+                                          <td>
+                                              <input name=Voornaam maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Geboortedatum </span>
+                                          </td>
+                                          <td>
+                                              <input name=Geboortedatum maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Geslacht</span>
+                                          </td>
+                                          <td>
+                                              <input type=radio value=Man name=Geslacht>
+                                              <span>Man&nbsp; </span>
+                                              <input type=radio value=Vrouw name=Geslacht>
+                                              <span>Vrouw&nbsp;</span>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>BSN / Sofinummer</span>
+                                          </td>
+                                          <td>
+                                              <input name=BSN maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                  </table>
+
+                                  <table>
+                                      <tr>
+                                          <td>
+                                              <span style='font-weight:bold;'>Adresgegevens</span>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Straatnaam </span>
+                                          </td>
+                                          <td>
+                                              <input name="TEKST_6" maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Huisnummer </span>
+                                          </td>
+                                          <td>
+                                              <input name="TEKST_7" maxlength=255 size=10 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Postcode </span>
+                                          </td>
+                                          <td>
+                                              <input name="TEKST_8" maxlength=255 size=10 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Woonplaats </span>
+                                          </td>
+                                          <td>
+                                              <input name="TEKST_9" maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Telefoon huis </span>
+                                          </td>
+                                          <td>
+                                              <input name="TEKST_10" maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Telefoon werk </span>
+                                          </td>
+                                          <td>
+                                              <input name="TEKST_11" maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Mobiel nummer</span>
+                                          </td>
+                                          <td>
+                                              <input name="TEKST_12" maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>E-mail adres</span>
+                                          </td>
+                                          <td>
+                                              <input name="TEKST_13" maxlength=255 size=46 value="" type=text>
+                                          </td>
+                                      </tr>
+                                  </table>
+
+                                  <table>
+                                      <tr>
+                                          <td>
+                                              <span style='font-weight:bold;'>Verzekeringsgegevens</span>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <span>Indien u de eerste keer de praktijk bezoekt, neemt u dan altijd uw verzekeringspasje mee.</span>
+                                          </td>
+                                          <td>
+                                              <span>&nbsp;</span>
+                                          </td>
+                                      </tr>
+                                      <tr>
+                                          <td>
+                                              <table>
+                                                  <tr>
+                                                      <td>
+                                                          <span>Naam verzekering:</span>
+                                                      </td>
+                                                      <td>
+                                                          <input name="TEKST_14" maxlength=255 size=44 value="" type=text>
+                                                      </td>
+                                                  </tr>
+                                                  <tr>
+                                                      <td>
+                                                          <span>Polisnummer:</span>
+                                                      </td>
+                                                      <td>
+                                                          <input name="TEKST_15" maxlength=255 size=44 value="" type=text>
+                                                      </td>
+                                                  </tr>
+                                                  <tr>
+                                                      <td>
+                                                          <span>Verzekering begindatum</span>
+                                                      </td>
+                                                      <td>
+                                                          <input name="TEKST_16" maxlength=255 size=44 value="" type=text>
+                                                      </td>
+                                                  </tr>
+                                              </table>
+
+                                              <table>
+                                                  <tr>
+                                                      <td>
+                                                          <span>Belangrijke gegevens / Opmerkingen<br>(verandering gezondheid en overige informatie)</span>
+                                                      </td>
+                                                  </tr>
+                                                  <tr>
+                                                      <td>
+                                                          <textarea name="TEKSTGEBIED_1" rows=5 cols=57></textarea>
+                                                      </td>
+                                                  </tr>
+                                              </table>
+
+                                              <span>&nbsp;</span>
+                                              <span>&nbsp;</span>
+                                              <p>
+                                                  <input type=submit value=Verzenden>
+                                                  <input type=submit value=Verzenden>
+                                              </p>
+
+                                             </div>
+
+                                          </td>
+                                      </tr>
+                                  </table>
+                              </td>
+                          </tr>
+                      </table>
+
+
+              </article>
+          
+          </section>
+          
+      </div>
+      
+    </div>
+      
+</section>
